3666fb276c70a59f5e2ac4874569c6f321bf744e
Kao <caolpgc60067@gmail.com>
2015-04-30 07:54:11 +0700

    #

--- View/ProductOptions/load_options.ctp
+++ View/ProductOptions/load_options.ctp
@@ -2,7 +2,7 @@
     <div class="panel-heading"><?php echo __('Options') ?></div>
     <div class="panel-body">
         <div class="list-group">
-            <div class="list-group-item">
+            <div class="list-group-item options">
                 <div class="row">
                     <div class="col-lg-12">
                         <?php
@@ -29,7 +29,7 @@
         </div>
     </div>
     <div class="hidden" id="template">
-        <div class="list-group-item">
+        <div class="list-group-item optionsdisabled">
             <div class="row">
                 <div class="col-lg-12">
                     <?php
@@ -37,7 +37,7 @@
                         $opts = Set::combine($optG, '{n}.Option.id', '{n}.Option.name');
                         ?>
                         <div class="col-md-2 input-group-sm">
-                            <?php echo $this->Form->input('Item.{{no}}.Option.', array('disabled'=>true,'options' => $opts, 'empty' => $key, 'class' => 'form-control input-sm', 'label' => false)); ?>
+                            <?php echo $this->Form->input('Item.{{no}}.Option.', array('disabled','options' => $opts, 'empty' => $key, 'class' => 'form-control input-sm', 'label' => false)); ?>
                         </div>
                     <?php
                     }
@@ -54,7 +54,7 @@
             </div>
         </div>
     </div>
-    <div class="panel-footer">
-            <a href="javascript:;" class="btn btn-primary">Thêm</a>
+    <div class="panel-footer text-right">
+            <a href="javascript:;" id="more-option" class="btn btn-primary">Thêm</a>
     </div>
 </div>
--- webroot/js/action.js
+++ webroot/js/action.js
@@ -1,63 +1,63 @@
-$(function() {
-    $('.currency').maskMoney({thousands:',', decimal:'.','precision':0, allowZero:false, suffix: ' VNĐ'}).focus();
+$(function () {
+    $('.currency').maskMoney({thousands: ',', decimal: '.', 'precision': 0, allowZero: false, suffix: ' VNĐ'}).focus();
 
-    $("#wareHouseModal").on("show.bs.modal", function(e) {
+    $("#wareHouseModal").on("show.bs.modal", function (e) {
         var link = $(this).data('href');
         var body = $(this).find(".modal-body");
         $.ajax(
             {
-                url : link,
-                success: function(data){
+                url: link,
+                success: function (data) {
                     body.html(data);
-                    setUpFormAjax($(body).find('form'),body);
-                    $('.currency').maskMoney({thousands:',', decimal:'.','precision':0, allowZero:false, suffix: ' VNĐ'}).focus();
+                    setUpFormAjax($(body).find('form'), body);
+                    $('.currency').maskMoney({thousands: ',', decimal: '.', 'precision': 0, allowZero: false, suffix: ' VNĐ'}).focus();
                 }
             }
         );
     });
 
-    $('.option-checkbox').each(function(){
+    $('.option-checkbox').each(function () {
         var id = $(this).val();
-        if( $(this).is(':checked')){
-            $('.parent-'+id).each(function(){
+        if ($(this).is(':checked')) {
+            $('.parent-' + id).each(function () {
                 $(this).closest('.icheckbox_flat').addClass('checked');
-                $(this).attr('checked','checked');
+                $(this).attr('checked', 'checked');
             })
-        }else{
-            $('.parent-'+id).each(function(){
+        } else {
+            $('.parent-' + id).each(function () {
                 $(this).closest('.icheckbox_flat').removeClass('checked');
                 $(this).removeAttr('checked');
             })
         }
     });
 
-    $('.checkbox ins, .checkbox label').on('click',function(){
+    $('.checkbox ins, .checkbox label').on('click', function () {
         var input = $(this).parent().find('.option-checkbox');
         var id = input.val();
-        if(input.is(':checked')){
-            $('.parent-'+id).each(function(){
+        if (input.is(':checked')) {
+            $('.parent-' + id).each(function () {
                 $(this).closest('.icheckbox_flat').addClass('checked');
-                $(this).attr('checked','checked');
+                $(this).attr('checked', 'checked');
             })
-        }else{
-            $('.parent-'+id).each(function(){
+        } else {
+            $('.parent-' + id).each(function () {
                 $(this).closest('.icheckbox_flat').removeClass('checked');
                 $(this).removeAttr('checked');
             })
         }
     });
 
-    $(document).on('change','#WarehouseProductId',function(){
-        if($(this).val()){
+    $(document).on('change', '#WarehouseProductId', function () {
+        if ($(this).val()) {
             var href = $('#warehouse-options').data('href');
-            $('#warehouse-options').load(href + '/' + $(this).val(),function(){
-                $('#warehouse-options input').iCheck({checkboxClass:'icheckbox_flat'});
+            $('#warehouse-options').load(href + '/' + $(this).val(), function () {
+                $('#warehouse-options input').iCheck({checkboxClass: 'icheckbox_flat'});
             });
-        }else{
+        } else {
             $('#warehouse-options').html('');
         }
     });
-    $(document).on('keydown','*[data-type=number]',function(e){
+    $(document).on('keydown', '*[data-type=number]', function (e) {
         // Allow: backspace, delete, tab, escape, enter and .
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
             // Allow: Ctrl+A
@@ -76,8 +76,30 @@ $(function() {
             e.preventDefault();
         }
     });
+
+    $(document).on('click', '#more-option', function () {
+
+
+        var uId = new Date().getTime() + Math.round(Math.random());
+        var template = $('#template').html();
+        template = template.replace(/{{no}}/g, uId);
+        template = template.replace(/disabled/g, '');
+        $('#warehouse-options .list-group').append(template);
+
+
+    });
+    $(document).on('change', '.options select', function () {
+        $('.options').each(function(index){
+            var vals = $(this).find('select').map(function () {
+                return this.value;
+            }).get().join(',');
+            if(index < $('.options').length -1){
+                console.log(vals);
+            }
+        });
+    });
 });
-var setUpFormAjax = function(form,div,popup, callback){
+var setUpFormAjax = function (form, div, popup, callback) {
     $(form).ajaxForm(
         {
             beforeSubmit: function (e) {
@@ -95,7 +117,7 @@ var setUpFormAjax = function(form,div,popup, callback){
             },
             complete: function (xhr) {
                 $(div).html(xhr.responseText);
-                setUpFormAjax($(div).find('form'),div,popup, callback);
+                setUpFormAjax($(div).find('form'), div, popup, callback);
             }
         }
     );
